99f9a93b420691224eea81f05d6f63e9202b086a
UserUserAdminAdmin <dmytro.chornyi.mitpa.2024@lpnu.ua>
2025-06-11 18:55:48 +0300

    v4

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def hello():
     return "Hello, CI/CD! version 3"
 
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json
+    print("Received webhook:", data)
+    return 'Webhook received!', 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
